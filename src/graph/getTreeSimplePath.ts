83a80f12645520a001e195b23e130cd57cb666a1
qlitre <kuri0403@gmail.com>
2023-04-01 01:07:09 +0900

    add docstring

--- src/graph/getTreeSimplePath.ts
+++ src/graph/getTreeSimplePath.ts
@@ -2,7 +2,14 @@ import { Deque } from "../datastructure/Deque";
 
 type Graph = Record<number, number[]>;
 
-
+/**
+ * 
+ * @param n 頂点数
+ * @param graph 隣接リスト
+ * @param startVertex 開始頂点
+ * @param endVertex 終了頂点
+ * @returns 最短距離のパスを返す
+ */
 export const getTreeSimplePath = (n: number, graph: Graph, startVertex: number, endVertex: number): number[] | -1 => {
     const vis = new Set<number>();
     const que = new Deque<number>()
